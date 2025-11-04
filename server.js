6a2da2bfc9bf1da923e548171a56054367d29c21
Ram Tharu <ramtharu532@gmail.com>
2025-11-04 13:58:14 +0545

    again update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { clerkMiddleware } from "@clerk/express";
-import { server } from "inngest/express";
+import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
 
 const app = express();
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
 
-app.use("/api/inngest", server({ client: inngest, functions }));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => {
   res.send("Server is running");
